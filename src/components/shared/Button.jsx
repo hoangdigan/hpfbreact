695aef6a62db05655b5fa4ea72fdc0c9b4400694
Hoang Pham <hoangdigan@gmail.com>
2022-09-30 06:39:13 +0700

    initial comm

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+function Button({children, version, type, isDisabled}) {
+  return (
+    <button 
+        type={type} 
+        disable={isDisabled}
+        className={`btn btn-${version}`}>
+        {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    version: 'primary',
+    type: 'button',
+    isDisaled: false
+}
+
+Button.prototype ={
+    children: PropTypes.node.isRequired,
+    version: PropTypes.string,
+    type: PropTypes.string,
+    isDisabled: PropTypes.bool
+}
+
+export default Button
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData= [
+    {
+       id:1,
+       rating:10,
+       text: 'it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ' 
+    },
+    {
+        id:2,
+        rating:9,
+        text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont' 
+     },
+     {
+        id:3,
+        rating:8,
+        text: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33' 
+     },
+]
+
+export default FeedbackData
